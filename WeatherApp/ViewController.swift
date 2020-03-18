874d013e0830a7adfadde62aad24a1361149ea23
Alex <88_n1@mail.ru>
2020-03-18 22:59:33 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by onyx on 18.03.2020.
+//  Copyright © 2020 Alex Al. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
